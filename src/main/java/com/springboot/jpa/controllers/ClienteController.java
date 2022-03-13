7f17eaae656f04318012ff34312780d31cd2cc16
victornavelino <victornavelino@gmail.com>
2022-03-13 20:51:28 -0300

    ABM de Clientes (sin eliminar)

--- src/main/java/com/springboot/jpa/controllers/ClienteController.java
+++ src/main/java/com/springboot/jpa/controllers/ClienteController.java
@@ -1,14 +1,24 @@
 package com.springboot.jpa.controllers;
 
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.springboot.jpa.models.dao.IClienteDao;
+import com.springboot.jpa.models.entity.Cliente;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 	
 	@Autowired
@@ -20,5 +30,39 @@ public class ClienteController {
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
+	
+	@RequestMapping(value ="/form")
+	public String crear(Map<String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("titulo", "Formulario de Cliente");
+		model.put("cliente", cliente);
+		return "form";
+	}
+	
+	@RequestMapping(value="/form", method = RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
+			return "form";			
+		}
+		clienteDao.save(cliente);
+		status.setComplete();
+		return "redirect:listar";
+	}
+	
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+		Cliente cliente=null;
+		if (id > 0) {
+			cliente=clienteDao.findOne(id);
+		} else {
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de Cliente");
+	return "form";	
+	}
+	
+	
 
 }
