c0b17577dc860d36a7c33027924e7f91f415f022
victornavelino <victornavelino@gmail.com>
2022-03-17 12:54:32 -0300

    mensajes

--- src/main/java/com/springboot/jpa/controllers/ClienteController.java
+++ src/main/java/com/springboot/jpa/controllers/ClienteController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.springboot.jpa.models.entity.Cliente;
 import com.springboot.jpa.models.service.IClienteService;
 
@@ -39,22 +41,24 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";			
 		}
 		clienteService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", "Cliente Creado Con Exito");
 		return "redirect:listar";
 	}
 	
 	@RequestMapping(value = "/form/{id}")
-	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente=null;
 		if (id > 0) {
 			cliente=clienteService.findOne(id);
 		} else {
+			flash.addFlashAttribute("error", "Cliente no encontrado");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
@@ -63,9 +67,10 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value = "/eliminar/{id}")
-	public String eliminar(@PathVariable(value = "id") Long id) {
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if (id > 0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente Eliminado Con Exito");
 		} 
 		
 		return "redirect:/listar";
