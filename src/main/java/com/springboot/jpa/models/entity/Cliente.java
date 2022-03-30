9f53b03f8bb2aaebc8806d645d6666839fff491a
victornavelino <victornavelino@gmail.com>
2022-03-30 12:56:43 -0300

    modelos itemfactura factura

--- src/main/java/com/springboot/jpa/models/entity/Cliente.java
+++ src/main/java/com/springboot/jpa/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.springboot.jpa.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -34,15 +39,23 @@ public class Cliente implements Serializable {
 	@NotEmpty
 	@Email
 	private String email;
-	
+
 	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
-	
+
+	@OneToMany(mappedBy ="cliente" ,fetch= FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+
 	private String foto;
+
 	
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -83,7 +96,6 @@ public class Cliente implements Serializable {
 		this.createAt = createAt;
 	}
 
-	
 	public String getFoto() {
 		return foto;
 	}
@@ -92,10 +104,16 @@ public class Cliente implements Serializable {
 		this.foto = foto;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public List<Factura> getFacturas() {
+		return facturas;
 	}
-	 
-	 
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
 
 }
---
+++ src/main/java/com/springboot/jpa/models/entity/Factura.java
@@ -0,0 +1,110 @@
+package com.springboot.jpa.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "facturas")
+public class Factura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotEmpty
+	private String descripcion;
+	@NotEmpty
+	private String observacion;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Cliente cliente;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id")
+	private List<ItemFactura> items;
+	
+	@PrePersist
+	public void prePersist() {
+		createAt= new Date();
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+}
