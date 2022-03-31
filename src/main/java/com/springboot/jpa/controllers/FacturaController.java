9ef7629af3796d22d1ec735d64833b59fc618280
victornavelino <victornavelino@gmail.com>
2022-03-31 20:59:04 -0300

    factura controler y formulario crear

---
+++ src/main/java/com/springboot/jpa/controllers/FacturaController.java
@@ -0,0 +1,39 @@
+package com.springboot.jpa.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.springboot.jpa.models.entity.Cliente;
+import com.springboot.jpa.models.entity.Factura;
+import com.springboot.jpa.models.service.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+	
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model, RedirectAttributes flash ) {
+		
+		Cliente cliente = clienteService.findOne(clienteId);
+		if(cliente==null) {
+			flash.addAttribute("error","El cliente no existe");
+			return "redirect:/listar";
+		}
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+		model.put("factura", factura);
+		model.put("titulo", "Crear Factura");
+		return "/factura/form";
+	}
+}
