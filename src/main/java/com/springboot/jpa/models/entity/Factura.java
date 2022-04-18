2b88cf78077bc6109b606a1a3938a9a839595535
victornavelino <victornavelino@gmail.com>
2022-04-18 20:03:02 -0300

    imports no usados en entidades

--- src/main/java/com/springboot/jpa/models/entity/Factura.java
+++ src/main/java/com/springboot/jpa/models/entity/Factura.java
@@ -19,12 +19,8 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "facturas")
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -41,5 +41,4 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2, 1, 4);
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 5);
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
 
-INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
 
