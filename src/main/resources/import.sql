27ecdfeff88939cb700b2e7edbe1713b5cbb90bc
victornavelino <victornavelino@gmail.com>
2022-04-18 19:49:41 -0300

    errror no anda

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -42,4 +42,4 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 5);
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
 
 INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
-INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
+
