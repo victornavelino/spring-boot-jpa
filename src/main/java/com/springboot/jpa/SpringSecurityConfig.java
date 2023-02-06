2874114f06303cd6d8dd28d7ba22588c7148c9c2
victornavelino <victornavelino@gmail.com>
2023-02-06 12:58:34 -0300

    spring security config

--- src/main/java/com/springboot/jpa/SpringSecurityConfig.java
+++ src/main/java/com/springboot/jpa/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -19,6 +20,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/css/**","/js/**", "/images/**", "/listar").permitAll()
+		.antMatchers("/ver/**").hasAnyRole("USER")
+		.antMatchers("/uploads/**").hasAnyRole("USER")
+		.antMatchers("/fomr/**").hasAnyRole("ADMIN")
+		.antMatchers("/form/**").hasAnyRole("ADMIN")
+		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+		.antMatchers("/factura/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+		.and()
+		.logout().permitAll();
+	}
+
+
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
 		
