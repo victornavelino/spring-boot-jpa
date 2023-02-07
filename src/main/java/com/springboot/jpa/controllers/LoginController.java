2f31b48f37b2bdd5e7008446d5f69555f20a86c4
hugo <victornavelino@gmail.com>
2023-02-07 19:40:45 -0300

    ´login modificado y controlador con error de login

--- src/main/java/com/springboot/jpa/controllers/LoginController.java
+++ src/main/java/com/springboot/jpa/controllers/LoginController.java
@@ -5,16 +5,21 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String login(Model model, Principal principal, RedirectAttributes flash) {
+	public String login(@RequestParam(value="error", required =false) String error,
+			Model model, Principal principal, RedirectAttributes flash) {
 		if (principal !=null) {
 			flash.addFlashAttribute("info", "El usuario ya se encuentra logueado");
 			return "redirect/";
+		}
+		if (error !=null) {
+			model.addAttribute("error", "Error en el login: usuario o contraseña incorrecta");
 		}
 		return "login";
 	}
