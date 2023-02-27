7471a58e167e15338f84f4615b0ad673c2ed2c35
victornavelino <victornavelino@gmail.com>
2023-02-27 13:03:03 -0300

    sadasdasdasdasdasdas

--- src/main/java/com/springboot/jpa/controllers/LoginController.java
+++ src/main/java/com/springboot/jpa/controllers/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String login(@RequestParam(value="error", required =false) String error,
+	public String login(@RequestParam(value="error", required =false) String error, @RequestParam(value="logout", required =false) String logout,
 			Model model, Principal principal, RedirectAttributes flash) {
 		if (principal !=null) {
 			flash.addFlashAttribute("info", "El usuario ya se encuentra logueado");
@@ -20,6 +20,9 @@ public class LoginController {
 		}
 		if (error !=null) {
 			model.addAttribute("error", "Error en el login: usuario o contraseña incorrecta");
+		}
+		if (logout !=null) {
+			model.addAttribute("success", "Su sesion se cerro con exito!");
 		}
 		return "login";
 	}
