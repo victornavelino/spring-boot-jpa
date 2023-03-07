fad6e4036344baba2437c8de5fd1bad1740f0112
victornavelino <victornavelino@gmail.com>
2023-03-07 12:49:15 -0300

    entidades rol y usuario

---
+++ src/main/java/com/springboot/jpa/models/entity/Role.java
@@ -0,0 +1,22 @@
+package com.springboot.jpa.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+
+@Table(name ="authorities")
+@Entity
+public class Role implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+
+}
---
+++ src/main/java/com/springboot/jpa/models/entity/Usuario.java
@@ -0,0 +1,17 @@
+package com.springboot.jpa.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name ="users")
+@Entity
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
