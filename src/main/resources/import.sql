a0648234b83e3bd146c1202a6babfcc67ebf55ac
hugo <victornavelino@gmail.com>
2023-03-13 20:15:36 -0300

    insert de roles y usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -43,3 +43,10 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
 
 INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
+
+INSERT INTO `users` (username, password, enabled) VALUES ('hugo','$2a$10$ClowEpvJOCdBGinw5CRc/eo0GXcVu9T5ZGrtzZPXsOF9O0St7Pmnu',1);
+INSERT INTO `users` (username, password, enabled) VALUES ('admin','$2a$10$MvIFQbkA1KDa3q5jwAB18uNd8i93FvCsw4kAfcBEM.Q4cO/OSZ922',1);
+
+INSERT INTO `authorities` (user_id, authority) VALUES (1,'ROLE_USER');
+INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_ADMIN');
+INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_USER');
