ed9d0b82a3957d2d8f4d7ebecc822a24b905e4a5
hugo <victornavelino@gmail.com>
2023-03-14 20:51:41 -0300

    internacionalizacion locale

--- src/main/java/com/springboot/jpa/MvcConfig.java
+++ src/main/java/com/springboot/jpa/MvcConfig.java
@@ -2,15 +2,20 @@ package com.springboot.jpa;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -39,4 +44,28 @@ public class MvcConfig implements WebMvcConfigurer {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+        
+        @Bean
+        public LocaleResolver localeResolver(){
+            SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+            localeResolver.setDefaultLocale(new Locale("es", "AR"));
+            return localeResolver;
+        }
+        
+        @Bean
+        public LocaleChangeInterceptor localeChangeInterceptor(){
+            LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+            localeInterceptor.setParamName("lang");
+            return localeInterceptor;
+        }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        
+       registry.addInterceptor(localeChangeInterceptor());
+    }
+        
+        
+        
+        
 }
