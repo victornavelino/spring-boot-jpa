17e7694242685e20a13242054aaaf946b0c38251
hugo <victornavelino@gmail.com>
2023-04-03 20:07:25 -0300

    Vista basada en clases Exportar a PDF factura

---
+++ src/main/java/com/springboot/jpa/view/pdf/FacturaPdfView.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.springboot.jpa.view.pdf;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author hugo
+ */
+@Component
+public class FacturaPdfView {
+    
+}
