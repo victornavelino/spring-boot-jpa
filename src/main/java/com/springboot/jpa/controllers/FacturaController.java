3c32f7c65645d0ccdd0e5c397bf99ce8c20b5aab
victornavelino <victornavelino@gmail.com>
2023-04-11 12:54:06 -0300

    facturacontroller ver factura y detalle

--- src/main/java/com/springboot/jpa/controllers/FacturaController.java
+++ src/main/java/com/springboot/jpa/controllers/FacturaController.java
@@ -37,7 +37,7 @@ public class FacturaController {
     private final Logger log = LoggerFactory.getLogger(getClass());
     
     @GetMapping("/ver/{id}")
-    public String ver(@PathVariable(value ="is") Long id, Model model, RedirectAttributes flash){
+    public String ver(@PathVariable(value ="id") Long id, Model model, RedirectAttributes flash){
         Factura factura = clienteService.findFacturaById(id);
         if (factura ==null) {
             flash.addFlashAttribute("error", "La factura no existe en la Base de datos!");
--- src/main/java/com/springboot/jpa/models/entity/Cliente.java
+++ src/main/java/com/springboot/jpa/models/entity/Cliente.java
@@ -28,92 +28,95 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Table(name = "clientes")
 public class Cliente implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@NotEmpty
-	private String nombre;
-	@NotEmpty
-	private String apellido;
-	@NotEmpty
-	@Email
-	private String email;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
+    @NotEmpty
+    @Email
+    private String email;
 
-	@NotNull
-	@Column(name = "create_at")
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date createAt;
+    @NotNull
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createAt;
 
-	@OneToMany(mappedBy ="cliente" ,fetch= FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Factura> facturas;
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Factura> facturas;
 
-	private String foto;
+    private String foto;
 
-	
-	public Cliente() {
-		facturas = new ArrayList<Factura>();
-	}
+    public Cliente() {
+        facturas = new ArrayList<Factura>();
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public String getApellido() {
+        return apellido;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public Date getCreateAt() {
-		return createAt;
-	}
+    public Date getCreateAt() {
+        return createAt;
+    }
 
-	public void setCreateAt(Date createAt) {
-		this.createAt = createAt;
-	}
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
 
-	public String getFoto() {
-		return foto;
-	}
+    public String getFoto() {
+        return foto;
+    }
 
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 
-	public List<Factura> getFacturas() {
-		return facturas;
-	}
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
 
-	public void setFacturas(List<Factura> facturas) {
-		this.facturas = facturas;
-	}
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
 
-	public void addFactura(Factura factura) {
-		facturas.add(factura);
-	}
+    public void addFactura(Factura factura) {
+        facturas.add(factura);
+    }
 
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
 }
