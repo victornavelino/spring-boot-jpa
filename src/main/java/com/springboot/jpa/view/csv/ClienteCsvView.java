b13ada809d7442e3761b5fd748d0e07b49ce38dd
hugo <victornavelino@gmail.com>
2023-05-02 19:58:23 -0300

    CSVVVVV

--- src/main/java/com/springboot/jpa/view/csv/ClienteCsvView.java
+++ src/main/java/com/springboot/jpa/view/csv/ClienteCsvView.java
@@ -4,11 +4,16 @@
  */
 package com.springboot.jpa.view.csv;
 
+import com.springboot.jpa.models.entity.Cliente;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 /**
  *
@@ -30,6 +35,14 @@ public class ClienteCsvView extends AbstractView {
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setHeader("content-disposition", "attachment; filename=\"clientes.csv\"");
         response.setContentType(getContentType());
+        Page<Cliente> clientes =(Page<Cliente>)model.get("clientes");
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] encabezado = {"id", "nombre", "apellido", "email", "createAt"};
+        beanWriter.writeHeader(encabezado);
+        for(Cliente cliente:clientes){
+            beanWriter.write(cliente, encabezado);
+        }
+        beanWriter.close();
     }
 
 }
